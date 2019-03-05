b5dc3dfb8cfd0e73c3489d82852c26a963a93ca3
bepitulaz <bepitulaz@gmail.com>
2019-03-05 08:13:50 +0000

    Publishing to gh-pages (publish.sh)

--- id/js/events.js
+++ id/js/events.js
@@ -6,4 +6,33 @@ $(document).ready(function() {
     $('#download-linux').click(function() {
         ga('send', 'event', 'Binaries', 'download', 'Download - Linux');
     });
+
+    // copy to clipboard
+    $('#copy-address').bind('click', function() {
+        var input = $('#eth-address');
+        var $temp = $("<input>");
+        $("body").append($temp);
+        $temp.val($(input).text()).select();
+        try {
+            var success = document.execCommand('copy');
+            if (success) {
+                $('#copy-address').trigger('copied', ['Copied!']);
+                $temp.remove();
+            } else {
+                $('#copy-address').trigger('copied', ['Copy with Ctrl-c']);
+                $temp.remove();
+            }
+        } catch (err) {
+            $('#copy-address').trigger('copied', ['Copy with Ctrl-c']);
+            $temp.remove();
+        }
+    });
+    
+    // Handler for updating the tooltip message.
+    $('#copy-address').bind('copied', function() {
+        $('#copy-address').text('Tersalin');
+        setTimeout(function() {
+            $('#copy-address').text('Salin Alamat');
+        }, 2000);
+    });
 });
