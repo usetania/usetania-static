64eb7cb772cca24bed18e2ea96a29d09b3d4a063
bepitulaz <bepitulaz@gmail.com>
2019-03-07 04:59:56 +0000

    Publishing to gh-pages (publish.sh)

--- hu/js/events.js
+++ hu/js/events.js
@@ -7,6 +7,10 @@ $(document).ready(function() {
         ga('send', 'event', 'Binaries', 'download', 'Download - Linux');
     });
 
+    $('#submit-contact-hu').click(function() {
+        ga('send', 'event', 'Contact', 'submit', 'Submit - Hungary');
+    });
+
     // copy to clipboard
     $('#copy-address').bind('click', function() {
         var input = $('#eth-address');
